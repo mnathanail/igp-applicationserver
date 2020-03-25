4f4493b46ac403a4ee9e6179c0dd4123fb2d7316
Manos Nathanail <m.nathanail@yahoo.gr>
2020-03-25 02:53:51 +0200

    test

--- diauloss/src/main/java/com/diaulos/igp/model/Cert.java
+++ diauloss/src/main/java/com/diaulos/igp/model/Cert.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "CERT")
@@ -14,7 +15,7 @@ public class Cert {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer companyId;
-
+	
 	@Column(name = "regulatedActivity")
 	private String regulatedActivity;
 	@Column(name = "document1")
