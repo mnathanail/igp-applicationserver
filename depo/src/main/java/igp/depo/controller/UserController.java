9a3fa9b2a866c3e55b90d510562abea311d7fb3f
Athanasios Tsagris <thanostsg@gmail.com>
2020-04-05 12:16:35 +0300

    Register/Login/Create

--- depo/src/main/java/igp/depo/controller/UserController.java
+++ depo/src/main/java/igp/depo/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,11 @@ public class UserController {
 	
 	@Autowired
 	private AitisiService aitisiService;
+	
+	@GetMapping("/")
+	public String index(){
+	return "silence is gold";
+	}
 	
 	@PostMapping("/newforeas")
 	public ResponseEntity<String> newForeas(@Valid @RequestBody ForeasModel foreas, BindingResult result){
